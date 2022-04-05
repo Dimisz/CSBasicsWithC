bf32207bad13acbadd2ed1f0edd1efcfa75a1823
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-05 17:30:15 +0300

    Using nested for loops to print multiplication table

--- Course2/forLoops.c
+++ Course2/forLoops.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-  
+  /*
   int upperBound;
   printf("Enter the upper bound:\n");
   scanf("%d", &upperBound);
@@ -12,6 +12,18 @@ int main(int argc, char** argv)
   {
     printf("The square of %d is %d\n", i, i*i);
   }
+  */
   
+  // print a multiplication table using nested for loops
+  printf("\nPrinting a multiplication table...\n");
+  for(int i = 1; i < 10; i++)
+  {
+    printf("%d\t", i);
+    for(int j = 1; j < 10; j++)
+    {
+      printf("%2d  ", i*j);
+    }
+    printf("\n");
+  }
   return (EXIT_SUCCESS);
 }
