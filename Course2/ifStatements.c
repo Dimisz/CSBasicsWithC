d07827730005575dfc01a36d744383d1bd055b36
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-05 16:11:04 +0300

    Added switch statement

--- Course2/ifStatements.c
+++ Course2/ifStatements.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
   char answer;
   
   // prompt for an answer
- 
+ /*
   while(1)
   {
     printf("Do you like pizza? (y or n)\n");
@@ -27,7 +27,22 @@ int main(int argc, char** argv)
       printf("You should enter either 'y' or 'n'\n");
     }
   }
-  
+  */
+  printf("Do you like pizza? (y or n)\n");
+  scanf("%c", &answer);
+  //answer = tolower(answer);
+  switch (answer) {
+    case 'y':
+      printf("Great! I also like pizza!\n\n");
+      break;
+    case 'n':
+      printf("That's ok, I'll eat enough for both of us.\n\n");
+      break;
+      
+    default:
+      printf("You should enter either 'y' or 'n'\n\n");
+      break;
+  }
   
   return (EXIT_SUCCESS);
 }
