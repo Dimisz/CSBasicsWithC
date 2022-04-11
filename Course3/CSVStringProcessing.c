200052b42bbda482a0b6eaff2a3b50b4750ba430
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-11 10:01:15 +0300

    processing csv

--- Course3/CSVStringProcessing.c
+++ Course3/CSVStringProcessing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(int argc, char** argv)
@@ -8,7 +9,7 @@ int main(int argc, char** argv)
   {
     float x;
     float y;
-  }
+  };
   typedef struct Vector Vector;
   
   Vector point;
@@ -40,7 +41,7 @@ int main(int argc, char** argv)
   
   // extract x from point string
   // char xString[commaIndex + 1]
-  char* xString = mmalloc((commaIndex + 1) * sizeof(char));
+  char* xString = malloc((commaIndex + 1) * sizeof(char));
   strncpy(xString, pointString, commaIndex);
   xString[commaIndex] = '\0';
   point.x = atof(xString);
@@ -49,6 +50,27 @@ int main(int argc, char** argv)
   free(xString);
   xString = NULL;
   
+  
+  // extract y
+  int length = strnlen(pointString, sizeof(pointString) - 1);
+  char* yString = malloc((length - commaIndex) * sizeof(char));
+  int offset = commaIndex + 1;
+  for(int i = 0; i < length - commaIndex -1; i++)
+  {
+    yString[i] = pointString[i + offset];
+  }
+  yString[length - commaIndex - 1] = '\0';
+  point.y = atof(yString);
+  
+  free(yString);
+  yString = NULL;
+  
+  
+  // print point info
+  printf("\n");
+  printf("Point x: %f\n", point.x);
+  printf("Point y: %f\n", point.y);
+  printf("\n");
   
   return (EXIT_SUCCESS);
 }
