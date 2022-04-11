895fc47c34e2d4694cf85bf8c3d8e2afee94b993
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-11 10:38:42 +0300

    Basic implementation of Caesar Cipher

--- Course3/CaesarCipher.c
+++ Course3/CaesarCipher.c
@@ -23,10 +23,57 @@ int main(int argc, char** argv)
     validMessage = true;
     for(int i = 0; i < originalMessageLength; i++)
     {
-      
+      if(!isupper(originalMessage[i]))
+      {
+        printf("\n");
+        printf("Message must contain only uppercase alphabetic characters!\n");
+        printf("Enter message to be encrypted (upper case alphabetic characters only):\n");
+        fgets(originalMessage, sizeof(originalMessage), stdin);
+        originalMessageLength = strnlen(originalMessage, sizeof(originalMessage)) - 1;
+        
+        // reset the flag and exit the for loop
+        validMessage = false;
+        break;
+      }
     }
   }
   
+  // build encrypted string (make sure it's null terminated)
+  // char encryptedMessage[originalMessageLength + 1]
+  char* encryptedMessage = malloc((originalMessageLength + 1) * sizeof(char));
+  for(int i = 0; i < originalMessageLength; i++)
+  {
+    encryptedMessage[i] = originalMessage[i] + SHIFT_AMOUNT;
+    if(encryptedMessage[i] > 'Z')
+    {
+      encryptedMessage[i] -= 'Z' - 'A' + 1;
+    }
+  }
+  encryptedMessage[originalMessageLength] = '\0';
+  
+  // build decrypted string (make sure it's null-terminated)
+  int encryptedMessageLength = originalMessageLength;
+  char* decryptedMessage = malloc((encryptedMessageLength + 1) * sizeof(char));
+  for(int i = 0; i < encryptedMessageLength; i++)
+  {
+    decryptedMessage[i] = encryptedMessage[i] - SHIFT_AMOUNT;
+    if(decryptedMessage[i] < 'A')
+    {
+      decryptedMessage[i] += 'Z' - 'A' + 1;
+    }
+  }
+  decryptedMessage[encryptedMessageLength] = '\0';
+  
+  
+  printf("\n");
+  printf("Original message: %s\n", originalMessage);
+  printf("Encrypted message: %s\n", encryptedMessage);
+  printf("Decrypted message: %s\n", decryptedMessage);
+  
+  free(encryptedMessage);
+  encryptedMessage = NULL;
+  free(decryptedMessage);
+  decryptedMessage = NULL;
   
   return (EXIT_SUCCESS);
 }
