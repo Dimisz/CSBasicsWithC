e0659484ff1d0d9e743d0f956d6c02a3fade2049
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-11 16:40:55 +0300

    Generating a random number

--- Course3/functions.c
+++ Course3/functions.c
@@ -3,14 +3,28 @@
 #include <time.h>
 
 int getRandom();
+float averageScores(int firstScore, int secondScore);
+float avgScoresInArray(int scores[], int count);
 
 int main(int argc, char** argv)
 {
+  /*
   srand(time(0));// initialize random number generator
   for(int i = 1; i <= 5; i++)
   {
     printf("Random nnumber %d: %d\n", i, getRandom());
   }
+   */
+  
+  // passing parameters
+  int score1 = 100;
+  int score2 = 99;
+  int scores[] = {98, 99, 100};
+  float averageScore = averageScores(score1, score2);
+  printf("Average of %d and %d is %.2f\n", score1, score2, averageScore);
+  
+  printf("Average of scores in aarray: %.2f", avgScoresInArray(scores, sizeof(scores)/ sizeof(scores[0])));
+  
   return (EXIT_SUCCESS);
 }
 
@@ -18,3 +32,21 @@ int getRandom()
 {
   return rand();
 }
+
+
+float averageScores(int firstScore, int secondScore)
+{
+  return (float)(firstScore + secondScore) / 2;
+}
+
+float avgScoresInArray(int scores[], int count)
+{
+  int sum = 0;
+  for(int i = 0; i < count; i++)
+  {
+    sum += scores[i];
+  }
+  return (float)sum / count;
+}
+
+
