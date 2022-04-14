7d6de8a3d4aef998321d23f98d17235bb2252786
Vladimir Solovyov <leilyngale@icloud.com>
2022-04-14 09:42:47 +0300

    implemented selection sort

--- Course4/sortingAlgorithms.c
+++ Course4/sortingAlgorithms.c
@@ -7,7 +7,7 @@
 
 
 void bubbleSort(int array[], int count);
-//void selectionSort(int array[], int count);
+void selectionSort(int array[], int count);
 //void insertionSort(int array[], int count);
 
 void getRandomArray(int array[]);
@@ -21,6 +21,7 @@ int main(int argc, char** argv){
   
   int values[NUM_VALUES];
   
+  /*
   //unsorted array
   getRandomArray(values);
   printArray("Unsorted array", values, NUM_VALUES);
@@ -29,6 +30,18 @@ int main(int argc, char** argv){
   bubbleSort(values, NUM_VALUES);
   printf("\n");
   printArray("Bubble sort results", values, NUM_VALUES);
+   */
+  
+  // selection sort
+  getRandomArray(values);
+  printf("\n");
+  printArray("Unsorted array", values, NUM_VALUES);
+  selectionSort(values, NUM_VALUES);
+  printf("\n");
+  printArray("Selection sort results", values, NUM_VALUES);
+  
+  
+  return (EXIT_SUCCESS);
 }
 
 void bubbleSort(int array[], int count){
@@ -55,3 +68,46 @@ void bubbleSort(int array[], int count){
     }
   }
 }
+
+
+void selectionSort(int array[], int count){
+  
+  if(PRINT_DEBUG_INFO){
+    printf("\n");
+    printf("Bubble sort\n");
+    printf("--------------\n");
+    printf("\n");
+  }
+  
+  // work from left to right
+  for(int k = 0; k < count; k++){
+    int minLocation = k;
+    
+    // traverse unsorted portion of the array
+    for(int i = k+1; i < count; i++){
+      if(PRINT_DEBUG_INFO){
+        printf("Comparing array[%d] (%d) to array[%d] (%d)\n", i,
+               array[i], minLocation, array[minLocation]);
+      }
+      
+      // check for swap
+      if(array[i] < array[minLocation]){
+        minLocation = i;
+        
+        if(PRINT_DEBUG_INFO){
+          printf("New min location: %d\n", minLocation);
+        }
+      }
+    }
+    // swap elements
+    swapElements(k, minLocation, array);
+    
+    if(PRINT_DEBUG_INFO){
+      printf("\n");
+      printf("After iteration %d: ", k + 1);
+      printArrayElements(array, count);
+    }
+  }
+}
+
+
